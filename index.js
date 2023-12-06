66ea9220201910e6f249601634ed7205d90f2a5f
Karin Portfolio <karin.abe@gmail.com>
2023-12-06 06:30:10 -0300

    envio do desafio 1

---
+++ index.js
@@ -0,0 +1,51 @@
+console.log("Ano novo está chegando!")
+console.log("----------")
+let cantora = "Mariah Carey"
+let idade = 54
+let anoNascimento = 2023 - 54
+console.log("A " + cantora +" nasceu no ano de " + anoNascimento + " e tem " + idade + " anos.")
+console.log("----------")
+let quartaFeira = true
+if(quartaFeira === true){
+console.log("Oba! Hoje é quarta-feira! Tem aula da Karynne!!!")
+}
+else("Ah... Hoje não é quarta-feira...")
+console.log("----------")
+console.log("O tipo de 'quartaFeira' é " + typeof quartaFeira + ".")
+console.log("----------")
+let id = 17
+if (id >=18){
+console.log("Você é maior de idade, poderá curtir o show do Roberto Carlos com participação especial de Mariah Carey")
+}
+else {
+    console.log("Você não tem idade suficiente. Sinto muito, mas assistirá ao show da tv globo.")
+}
+
+console.log("----------")
+
+console.log("Sobre o mês de aniversário: ") 
+
+let Agosto = true
+let Dezembro = false
+let Junho = false
+let mes
+
+if (Agosto || Dezembro|| Junho === true){
+    console.log("Uma ou mais opções estão corretas.")
+}
+else{
+    console.log("Algo de errado não está certo")
+}
+
+if(mes = Agosto){
+console.log("O mês escolhido foi Agosto") 
+}
+else if (mes = Dezembro){
+    console.log("O mês escolhido foi Dezembro") 
+    }
+else if (mes = Junho){
+    console.log("O mês escolhido foi Junho") 
+    }
+    else{
+        console.log("valor invalido")
+    }
